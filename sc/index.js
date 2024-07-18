f29f3788111ddc2b7f6fc2717586aef1472c5fd3
kiddykidkid <amoduamodu28@gmail.com>
2024-07-17 22:01:48 -0700

    Create index.js

---
+++ sc/index.js
@@ -0,0 +1,57 @@
+let transitionActive = false;
+var song = document.getElementById("song");
+var current_page = "main";
+
+function startHome() {
+    if (transitionActive) return;
+    transitionActive = true;
+
+    const displayText = document.querySelector('.display-text');
+    displayText.style.opacity = 0;
+
+    setTimeout(() => {
+        displayText.innerHTML = getRandomSentence();
+        displayText.style.opacity = 1;
+        transitionActive = false;
+    }, 2000);
+}
+
+
+function startPage() {
+    const enterButton = document.getElementById("enter-container");
+    enterButton.style.transition = "opacity 1s ease-in-out";
+    enterButton.style.opacity = 0;
+
+    setTimeout(() => {
+        setTimeout(() => {
+            const memberButtons = document.getElementById("member-container");
+            const mainButtons = document.getElementById("main-container");
+            memberButtons.style.display = "flex";
+            mainButtons.style.display = "flex";
+
+            requestAnimationFrame(() => {
+                memberButtons.style.transition = "opacity 1s ease-in-out";
+                memberButtons.style.opacity = 1;
+                mainButtons.style.transition = "opacity 1s ease-in-out";
+                mainButtons.style.opacity = 1;
+            });
+
+            enterButton.style.display = "none";
+        }, 500);
+    }, 1000);
+    
+    song.volume = 0;
+    song.playbackRate = 0.85;
+    song.play();
+
+    var fadeInInterval = setInterval(function () {
+        song.volume += 0.1;
+        if (song.volume >= 0.7) {
+            song.volume = 0.7;
+            clearInterval(fadeInInterval);
+        }
+    }, 350);
+
+    setTimeout(startHome, 1000);
+};
+
